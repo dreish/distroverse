3553281e2abc2c8e5c4b2c8c4236c5ba7ac3587c
Dan Reish <dreish@distroverse.org>
2008-03-02 19:30:59 -0500

    Added comments to define function of abstract handle*() methods in DvtpServer.  Filled in HelloServer a little more as part of an effort to gain an understanding of how the server pieces will fit together by designing from the top down.

--- src/org/distroverse/distroplane/lib/DvtpServer.java
+++ src/org/distroverse/distroplane/lib/DvtpServer.java
@@ -35,8 +35,28 @@ public abstract class DvtpServer
       mListener.serve();
       }
       
+   /**
+    * Handles the LOCATION command, which tells a client the URL of the
+    * server that should be used for that location URL.  It must return
+    * the URL of a Java .jar file (various compressed jar extensions are
+    * recognized) or a Scheme source file with an URL ending in ".szp".
+    * @param location - an URL
+    */
    public abstract void handleLocation( String location );
+   /**
+    * Get an arbitrary resource by URL.  This mimics the GET method of
+    * an HTTP server to some extent, but a DVTP server does not ever
+    * implement a POST method, and a DVTP client should not accept
+    * http://... URLs as user input.
+    * @param url - an URL
+    */
    public abstract void handleGet( String url );
+   /**
+    * Handles the PROXYOPEN command, which is the handshake that begins
+    * a session between proxy and server.  The response, and every
+    * aspect of the protocol after that point, are completely 
+    * @param token
+    */
    public abstract void handleProxyOpen( String token );
 
    int          mListenPort;
--- src/org/distroverse/helloserver/HelloServer.java
+++ src/org/distroverse/helloserver/HelloServer.java
@@ -1,20 +1,59 @@
 package org.distroverse.helloserver;
 
+import org.distroverse.distroplane.lib.*;
+
 /**
  * An ultra-simple server to demonstrate the basic concepts of DVTP.
  * 
  * @author dreish
  */
-public final class HelloServer
+public final class HelloServer extends DvtpServer
    {
+   public HelloServer( DvtpListener l )
+      {
+      super( l );
+      }
+   
+   /* (non-Javadoc)
+    * @see org.distroverse.distroplane.lib.DvtpServer#handleGet(java.lang.String)
+    */
+   @Override
+   public void handleGet( String url )
+      {
+      // TODO Auto-generated method stub
+      
+      }
+
+   /* (non-Javadoc)
+    * @see org.distroverse.distroplane.lib.DvtpServer#handleLocation(java.lang.String)
+    */
+   @Override
+   public void handleLocation( String location )
+      {
+      // TODO Auto-generated method stub
+      
+      }
+
+   /* (non-Javadoc)
+    * @see org.distroverse.distroplane.lib.DvtpServer#handleProxyOpen(java.lang.String)
+    */
+   @Override
+   public void handleProxyOpen( String token )
+      {
+      // TODO Auto-generated method stub
+      
+      }
+
    /**
     * @param args
     */
    public static void main( String[] args )
       {
       // TODO Auto-generated method stub
-      DvtpListener l = new DvtpListener();
-      l.
+      DvtpListener l = new DvtpMultiplexedListener();
+      HelloServer  h = new HelloServer( l );
       l.serve();
       }
+   
+   
    }
