fdfe8964ac8da3ebaa7e644906786f59e625a076
Dan Reish <dreish@distroverse.org>
2008-03-02 19:30:06 -0500

    Adding an attempt at a function taking two arguments and returning one.

---
+++ src/org/distroverse/core/FoldingFunction.java
@@ -0,0 +1,8 @@
+package org.distroverse.core;
+
+public abstract class FoldingFunction
+   {
+   @SuppressWarnings("unused")
+   public static <T> T function( T a, T b )
+      { return null; }
+   }
