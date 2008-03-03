9e6988aea25ecf96001f404be9f12d0baff36d44
Dan Reish <dreish@distroverse.org>
2008-03-02 19:30:48 -0500

    Added some incomplete stuff.  Trying to design a DVTP server.

---
+++ src/org/distroverse/distroplane/lib/DvtpServer.java
@@ -0,0 +1,22 @@
+/**
+ * An abstract base class handling the annoying details of a DVTP
+ * server.
+ */
+package org.distroverse.distroplane.lib;
+
+/**
+ * @author dreish
+ *
+ */
+public abstract class DvtpServer
+   {
+   /**
+    * 
+    */
+   public DvtpServer()
+      {
+      // TODO Auto-generated constructor stub
+      }
+
+   
+   }
---
+++ src/org/distroverse/helloserver/HelloServer.java
@@ -0,0 +1,20 @@
+package org.distroverse.helloserver;
+
+/**
+ * An ultra-simple server to demonstrate the basic concepts of DVTP.
+ * 
+ * @author dreish
+ */
+public final class HelloServer
+   {
+   /**
+    * @param args
+    */
+   public static void main( String[] args )
+      {
+      // TODO Auto-generated method stub
+      DvtpListener l = new DvtpListener();
+      l.
+      l.listen();
+      }
+   }
