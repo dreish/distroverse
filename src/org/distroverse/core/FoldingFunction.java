e5a3a9977e2f605d4a32503094589a72fd5946c2
Dan Reish <dreish@distroverse.org>
2008-03-02 19:30:13 -0500

    Folded FoldingFunction into Util.

--- src/org/distroverse/core/FoldingFunction.java
+++
@@ -1,8 +0,0 @@
-package org.distroverse.core;
-
-public abstract class FoldingFunction
-   {
-   @SuppressWarnings("unused")
-   public static <T> T function( T a, T b )
-      { return null; }
-   }
