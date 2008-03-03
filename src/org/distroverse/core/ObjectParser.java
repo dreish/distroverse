0825fe8a5faab6247d35470c4641cf316cba8e06
Dan Reish <dreish@distroverse.org>
2008-03-02 19:31:32 -0500

    More object parser code.

--- src/org/distroverse/core/ObjectParser.java
+++ src/org/distroverse/core/ObjectParser.java
@@ -12,11 +12,42 @@ import java.nio.ByteBuffer;
  */
 public abstract class ObjectParser< T >
    {
+   public ObjectParser( ByteBuffer b )
+      {
+      super();
+      mBuffer = b;
+      mBaos = new ByteArrayOutputStream();
+      }
+ 
    public void setQueue( NetInQueue< T > queue )
       {
       mQueue = queue;
       mPosition = 0;
       }
+   
+   /**
+    * Takes input (from the network socket), adds it to the variable-
+    * size buffer mBaos, and if there is at least one complete object
+    * in that buffer ready to be reassembled, adds that object or
+    * objects to the queue, removes it or them from the buffer, and
+    * notify()s any wait()ing threads.
+    * @param input - a ByteBuffer, all of which is to be copied
+    */
+   synchronized public void
+   readBytes( ByteBuffer input )
+   throws Exception
+      {
+      if ( ! input.hasArray() )
+         throw new IllegalArgumentException( "input with no array" );
+      mBaos.write( input.array(), input.arrayOffset(),
+                   input.capacity() );
+      parseObjects( mBaos, mQueue );
+      }
+   
+   abstract protected void
+   parseObjects( ByteArrayOutputStream baos,
+                 NetInQueue< T > queue )
+   throws Exception;
 
    private NetInQueue< T >       mQueue;
    private int                   mPosition;
