c546320aebd1243667f85ecf32e73fa28277bc34
Dan Reish <dreish@distroverse.org>
2008-03-02 19:31:53 -0500

    Added some more unimplemented GUI classes. Changed one idea about the protocol.

---
+++ src/org/distroverse/viewer/gui/PanelHider.java
@@ -0,0 +1,13 @@
+package org.distroverse.viewer.gui;
+
+import com.jme.scene.Node;
+
+public class PanelHider extends Button
+   {
+   public PanelHider( Node parent )
+      {
+      super( parent );
+      // TODO Auto-generated constructor stub
+      }
+
+   }
---
+++ src/org/distroverse/viewer/gui/ScrollBar.java
@@ -0,0 +1,14 @@
+package org.distroverse.viewer.gui;
+
+import com.jme.scene.Node;
+
+public class ScrollBar extends Element
+   {
+   public ScrollBar( Node parent )
+      {
+      super( parent );
+      // TODO Auto-generated constructor stub
+      }
+   
+   private double mPosition;
+   }
