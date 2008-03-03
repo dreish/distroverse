9943a646d5e663d2e6f40be1bd5698e2221d8d28
Dan Reish <dreish@distroverse.org>
2008-03-02 19:32:04 -0500

    Log also needs to be LGPL.

--- src/org/distroverse/core/Log.java
+++ src/org/distroverse/core/Log.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2007 Dan Reish.
+ * 
+ * For license details, see the file COPYING in your distribution,
+ * or the <a href="http://www.gnu.org/copyleft/gpl.html">GNU
+ * Lesser General Public License (GPL) version 3 or later</a>
+ */
 package org.distroverse.core;
 
 public class Log
