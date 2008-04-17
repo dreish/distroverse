a63075cd58dadc01eb2f7c62f811332aaa331863
Dan Reish <dreish@distroverse.org>
2008-04-17 19:18:00 -0400

    Made parseObjects() loop until all available objects are parsed.
    (Still just handles strings.)

--- src/org/distroverse/core/net/DvtpFlexiParser.java
+++ src/org/distroverse/core/net/DvtpFlexiParser.java
@@ -25,44 +25,53 @@ public class DvtpFlexiParser extends ObjectParser< Object >
       {
       // FIXME This is a grossly suboptimal implementation, and
       // FIXME (second opinion) it's ugly too
-      mNextObject.write( baos.toByteArray() );
-      byte[] next_object = mNextObject.toByteArray();
+      boolean cont = true;
+      while ( cont )
+         {
+         mNextObject.write( baos.toByteArray() );
+         byte[] next_object = mNextObject.toByteArray();
       
-      if ( beginsWithNul( next_object ) )
-         {
-         // This is an arbitrary DvtpExternalizable object.  Find the
-         // length and find out whether we have the whole thing yet.
-         int ob_len = objectLength( next_object );
-         if ( ob_len > 0  &&  ob_len <= next_object.length )
+         if ( beginsWithNul( next_object ) )
             {
-            // XXX Convert and add to the queue.
-            
-            // XXX Delete parsed object from mNextObject.
-            }
-         }
-      else
-         {
-         // This is a raw string.  Find out whether we have the newline
-         // terminator yet.
-         for ( int i = 0; i < next_object.length - 1; ++i )
-            if (    next_object[ i   ] == '\r' 
-                 && next_object[ i+1 ] == '\n' )
+            // This is an arbitrary DvtpExternalizable object.  Find the
+            // length and find out whether we have the whole thing yet.
+            int ob_len = objectLength( next_object );
+            if ( ob_len > 0  &&  ob_len <= next_object.length )
                {
-               // Yes.
-               byte[] string_part = new byte[ i ];
-               System.arraycopy( next_object, 0, string_part, 0, i );
-               ByteArrayOutputStream string_baos
-                  = new ByteArrayOutputStream();
-               string_baos.write( string_part );
-               queue.add( string_baos.toString( "UTF-8" ) );
-               
-               byte[] remainder = new byte[ next_object.length
-                                            - i - 2 ];
-               System.arraycopy( next_object, i + 2, remainder, 0,
-                                 next_object.length - i - 2 );
-               baos.reset();
-               baos.write( remainder );
+               // XXX Convert and add to the queue.
+            
+               // XXX Delete parsed object from mNextObject.
                }
+            }
+         else
+            {
+            // This is a raw string.  Find out whether we have the
+            // newline terminator yet.
+            cont = false;
+            for ( int i = 0; i < next_object.length - 1; ++i )
+               if (    next_object[ i   ] == '\r' 
+                    && next_object[ i+1 ] == '\n' )
+                  {
+                  // Yes.
+                  byte[] string_part = new byte[ i ];
+                  System.arraycopy( next_object, 0, string_part, 0, i );
+                  ByteArrayOutputStream string_baos
+                     = new ByteArrayOutputStream();
+                  string_baos.write( string_part );
+                  queue.add( string_baos.toString( "UTF-8" ) );
+               
+                  byte[] remainder = new byte[ next_object.length
+                                               - i - 2 ];
+                  System.arraycopy( next_object, i + 2, remainder, 0,
+                                    next_object.length - i - 2 );
+                  mNextObject.reset();
+                  mNextObject.write( remainder );
+                  
+                  if ( remainder.length > 0 )
+                     cont = true;
+                  break;
+                  }
+            }
          }
       }
 
@@ -79,6 +88,7 @@ public class DvtpFlexiParser extends ObjectParser< Object >
       {
       try
          {
+         // FIXME Probably don't want to use a CompactUlong here
          long len = CompactUlong.externalAsLong( 
                        Util.baToObjectInput( object ) );
          return Util.safeInt( len );
