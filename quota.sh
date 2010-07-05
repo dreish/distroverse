5925e3ad707b10f9f3ed580b3f45448757ddebf3
Dan Reish <dreish@distroverse.org>
2010-07-04 20:59:06 -0400

    Starting over from scratch

--- quota.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-echo `date` '     ' `git diff -M --ignore-space-change master | wc -l` \(`git diff -M --ignore-space-change master\^ | wc -l` \(`git diff -M --ignore-space-change master\^\^ | wc -l` \) \)
-sleep 600
-done
