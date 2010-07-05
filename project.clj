f3ffe189d632bfbabaf35034dc0a98fd67ad22aa
Dan Reish <dreish@distroverse.org>
2010-07-04 22:23:26 -0400

    Filled out project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject distroverse "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+(defproject distroverse "0.1.0-SNAPSHOT"
+  :description "A distributed virtual reality system"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :repositories {"clojure-releases"
+                 "http://build.clojure.org/releases"})
