b5923c10d874ea7708fcf8f806f6b95f8c2997b1
Dan Reish <dreish@distroverse.org>
2010-08-28 20:33:57 -0400

    Switched to local bux-fixed version of native-deps
    My code appears substantially the same as the version tagged 1.0.3 in the
    native-deps git repository, but the native-deps 1.0.3 jar doesn't work for
    me.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [penumbra "0.6.0-SNAPSHOT"]]
   :native-dependencies [[penumbra/lwjgl "2.4.2"]]
-  :dev-dependencies [[native-deps "1.0.1"]
+  :dev-dependencies [[native-deps "1.0.2dr1"]
                      [swank-clojure "1.2.0"]
                      [midje "0.4.0"]]
   :repositories {"clojure-releases"
