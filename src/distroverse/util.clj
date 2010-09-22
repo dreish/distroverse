4ded737de1b7a5598569d051386f05a9588874fb
Dan Reish <dreish@distroverse.org>
2010-09-21 22:26:48 -0400

    Moved RawOps out to a hacky assembler thing
    This is probably not the right way to do this, but I don't feel like prying
    open leiningen to find out how to build a .class as a prerequisite to
    compiling everything else.

--- src/distroverse/util.clj
+++ src/distroverse/util.clj
@@ -10,109 +10,9 @@
 
 
 (ns distroverse.util
+  (:use [distroverse.assem raw-ops])
   (:import [java.net URL URI]
-           [clojure.asm ClassWriter Opcodes]))
-
-
-(defn define-java-class-from-bytecode
-  ([classname bytecode]
-     (.defineClass @clojure.lang.Compiler/LOADER classname bytecode nil)))
-
-;;; Using ASM, define a Java class containing raw, unchecked
-;;; operations not currently offered by Clojure.  This would be easier
-;;; to write in Java, but I refuse to do that on principle.
-
-(define-java-class-from-bytecode "distroverse.util.RawOps"
-  (let
-      [public Opcodes/ACC_PUBLIC
-       static Opcodes/ACC_STATIC
-       lload Opcodes/LLOAD
-       iload Opcodes/ILOAD
-       cw (ClassWriter. 0)]
-    (.visit cw Opcodes/V1_5 (+ public Opcodes/ACC_SUPER)
-            "distroverse/util/RawOps" nil "java/lang/Object" nil)
-    (doto (.visitMethod cw public "<init>" "()V" nil nil)
-      (.visitVarInsn Opcodes/ALOAD 0)
-      (.visitMethodInsn Opcodes/INVOKESPECIAL "java/lang/Object"
-                        "<init>" "()V")
-      (.visitInsn Opcodes/RETURN)
-      (.visitMaxs 1 1)
-      (.visitEnd))
-    (doseq [ [mname op] [ ["xor" Opcodes/LXOR]
-                          ["and" Opcodes/LAND]
-                          ["or"  Opcodes/LOR ] ] ]
-      (doto (.visitMethod cw (+ public static)
-                          mname "(JJ)J" nil nil)
-        (.visitVarInsn lload 0)
-        (.visitVarInsn lload 2)
-        (.visitInsn op)
-        (.visitInsn Opcodes/LRETURN)
-        (.visitMaxs 4 4)
-        (.visitEnd)))
-    (doseq [ [mname op] [ ["shiftLeft"  Opcodes/LSHL]
-                          ["shiftRight" Opcodes/LSHR] ] ]
-      (doto (.visitMethod cw (+ public static)
-                          mname "(JI)J" nil nil)
-        (.visitVarInsn lload 0)
-        (.visitVarInsn iload 2)
-        (.visitInsn op)
-        (.visitInsn Opcodes/LRETURN)
-        (.visitMaxs 3 3)
-        (.visitEnd)))
-    (doseq [ [mname op] [ ["xor"        Opcodes/IXOR]
-                          ["and"        Opcodes/IAND]
-                          ["or"         Opcodes/IOR ]
-                          ["shiftLeft"  Opcodes/ISHL]
-                          ["shiftRight" Opcodes/ISHR] ] ]
-      (doto (.visitMethod cw (+ public static)
-                          mname "(II)I" nil nil)
-        (.visitVarInsn iload 0)
-        (.visitVarInsn iload 1)
-        (.visitInsn op)
-        (.visitInsn Opcodes/IRETURN)
-        (.visitMaxs 2 2)
-        (.visitEnd)))
-    (doseq [intype ["J" "I" "S" "B"]
-            outype ["J" "I" "S" "B"]]
-      (when (not= intype outype)
-        (let [mname ({"B" "byteCast"
-                      "S" "shortCast"
-                      "I" "intCast"
-                      "J" "longCast"} outype)
-              inlen  ({"J" 8, "I" 4, "S" 2, "B" 1} intype)
-              outlen ({"J" 8, "I" 4, "S" 2, "B" 1} outype)]
-          (let [mv (.visitMethod cw (+ public static) mname
-                                 (str "(" intype ")" outype)
-                                 nil nil)]
-            (if (= intype "J")
-              (.visitVarInsn mv lload 0)
-              (.visitVarInsn mv iload 0))
-            (when (and (= inlen 8)
-                       (< outlen 8))
-              (.visitInsn mv Opcodes/L2I))
-            (when (and (> inlen 1)
-                       (= outlen 1))
-              (.visitInsn mv Opcodes/I2B))
-            (when (and (> inlen 2)
-                       (= outlen 2))
-              (.visitInsn mv Opcodes/I2S))
-            (when (and (< inlen 8)
-                       (= outlen 8))
-              (.visitInsn mv Opcodes/I2L))
-            (if (= outype "J")
-              (.visitInsn mv Opcodes/LRETURN)
-              (.visitInsn mv Opcodes/IRETURN))
-            (.visitMaxs mv
-                        (if (or (= outlen 8)
-                                (= inlen 8))
-                          2 1)
-                        (if (= inlen 8)
-                          2 1))
-            (.visitEnd mv)))))
-    (.visitEnd cw)
-    (.toByteArray cw)))
-
-(import 'distroverse.util.RawOps)
+           [distroverse.assem RawOps]))
 
 (defn unchecked-byte
   "Coerces to byte without checking for numeric overflow"
@@ -127,7 +27,7 @@
         ~(str "Coerces to " primtype
               " without checking for numeric overflow")
         ([~'n]
-           `(. distroverse.util.RawOps ~'~(symbol (str primtype "Cast"))
+           `(. distroverse.assem.RawOps ~'~(symbol (str primtype "Cast"))
                ~~'n)))))
 
 (doseq [t ["byte" "short" "int" "long"]]
@@ -140,7 +40,7 @@
      `(defmacro ~(symbol (str "raw-" op))
         ~(str "Performs " article " " op " on numeric primitives")
         ([~'a ~'b]
-           `(. distroverse.util.RawOps ~'~mname
+           `(. distroverse.assem.RawOps ~'~mname
                ~~'a ~~'b)))))
 
 (doseq [ [article op mname] [ ["an" "xor" 'xor]
