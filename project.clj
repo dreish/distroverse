9eb8aa16f44bf183e21713ce71ebd930e756f118
Dan Reish <dreish@distroverse.org>
2010-09-25 18:36:48 -0400

    Upgraded to native-deps 1.0.4, set :disable-implicit-clean in project.clj
    (:disable-implicit-clean makes the uberjar buildable at least, but the
    uberjar is not currently usable due to a problem with native libs.)

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [penumbra "0.6.0-SNAPSHOT"]]
   :native-dependencies [[penumbra/lwjgl "2.4.2"]]
-  :dev-dependencies [[native-deps "1.0.2dr1"]
+  :dev-dependencies [[native-deps "1.0.4"]
                      [swank-clojure "1.2.0"]
                      [midje "0.4.0"]]
   :repositories {"clojure-releases"
                  "http://build.clojure.org/releases"}
-  :main distroverse.core)
+  :main distroverse.core
+  :disable-implicit-clean true)
